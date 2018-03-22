c98a4155c273ccbe8cdb0cd4ab8f90d39637ba01
danielhkim92 <danielhkim92@gmail.com>
2018-03-22 17:00:57 -0500

    redid the components as const instead ofdoing it tthe toher way

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,18 +1,16 @@
 import React from "react";
 
-class Weather extends React.Component {
-	render(){
-		return (
-			<div>
-				{ this.props.city && <p>City: {this.props.city}</p> }
-				{ this.props.country && <p>Country: {this.props.country}</p> }
-				{ this.props.temperature && <p>Temperature: {this.props.temperature}</p> }
-				{ this.props.humidity && <p>Humidity: {this.props.humidity}</p> }
-				{ this.props.description && <p>Conditions: {this.props.description}</p> }
-				{ this.props.error && <p>{this.props.error}</p> }
-			</div>
-			);
-	}
-};
+const Weather = (props) => {
+	return (
+		<div>
+			{ props.city && <p>City: {props.city}</p> }
+			{ props.country && <p>Country: {props.country}</p> }
+			{ props.temperature && <p>Temperature: {props.temperature}</p> }
+			{ props.humidity && <p>Humidity: {props.humidity}</p> }
+			{ props.description && <p>Conditions: {props.description}</p> }
+			{ props.error && <p>{props.error}</p> }
+		</div>
+	);
+}
 
 export default Weather;
