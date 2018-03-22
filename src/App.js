da998cf8dc4537e52f01f97d6c56c4062c614acc
danielhkim92 <danielhkim92@gmail.com>
2018-03-22 16:20:09 -0500

    finished wrtiing afetch api call

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Weather from "./components/Weather";
 const API_KEY = "de2843400cabb101e11b7146f4f0afbe";
 
 class App extends React.Component {
+  getWeather = async () => {`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+  const data = await api_call.json();
+  console.log(data);
+  }
   render() {
     return (
       <div>
